9ccdf0c2e866eda626a52f943884cf3e3171da5e
shnmrzpgs <145262229+shnmrzpgs@users.noreply.github.com>
2025-01-10 13:54:09 +0800

    Temporary update of livewire components, forms and blade for editing of user account

--- app/Livewire/Forms/EditUserAccountForm.php
+++ app/Livewire/Forms/EditUserAccountForm.php
@@ -4,8 +4,10 @@ namespace App\Livewire\Forms;
 
 use Livewire\Form;
 use Illuminate\Support\Facades\DB;
+use App\Models\UserProfilePhoto;
 use Illuminate\Validation\Rules\Password;
 use Exception;
+use Illuminate\Support\Facades\Storage;
 
 class EditUserAccountForm extends Form
 {
@@ -20,105 +22,118 @@ class EditUserAccountForm extends Form
     public ?string $date_of_birth = null;
 
     public string $email = '';
-
-  //   // public string $id_number = '';
-  //   public string $position = '';
-
     //Permission
 
-    public ?string $user_role = null;
+    public ?string $role = null;
 
     public string $password = '';
 
     public bool $user_status = true;
+    public $staff;
     public $form;
+    public $photo;
     public $photo_path;
     public $user;
 
-  public function mount($user): void
-  {
-      $this->user = $user;
-      $this->first_name = $user->first_name;
-      $this->middle_name = $user->middle_name;
-      $this->last_name = $user->last_name;
-      $this->sex = $user->sex;
-      $this->date_of_birth = $user->date_of_birth;
-      $this->email = $user->email;
-      $this->user_role = $user->userRole->user_role;
-      $this->user_status = $user->user_status;
-  }
+    public function mount($staff): void
+    {
+        $this->staff = $staff;
+        $this->first_name = $staff->user->first_name;
+        $this->middle_name = $staff->user->middle_name;
+        $this->last_name = $staff->user->last_name;
+        $this->sex = $staff->user->sex;
+        $this->date_of_birth = $staff->user->date_of_birth;
+        $this->email = $staff->user->email;
 
-  public function rules(): array
-  {
-      return [
-          'first_name' => ['required', 'string'],
-          'middle_name' => ['nullable', 'string'],
-          'last_name' => ['required', 'string'],
-          'sex' => ['required', 'in:male,female'],
-          'date_of_birth' => ['nullable', 'date', 'date_format:Y-m-d'],
-          'email' => ['required', 'email', 'unique:users,email'],
-          'password' => ['required', 'string', Password::default()],
-      ];
-  }
+        $this->user_status = $staff->user_status;
+    }
 
-  public function messages(): array
-  {
-      return [
-          // Basic information
-          'first_name.required' => 'The first name field is required.',
-          'middle_name.string' => 'The middle name field must be a string.',
-          'last_name.required' => 'The last name field is required.',
-          'sex.required' => 'The gender field is required.',
-          'email.required' => 'The email field is required.',
-          'email.email' => 'The email field must be a valid email.',
-          'email.unique' => 'The email field has already been taken.',
-          'email.exists' => 'The email field is invalid.',
-          'password.required' => 'The password field is required.',
-      ];
-  }
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string'],
+            'middle_name' => ['nullable', 'string'],
+            'last_name' => ['required', 'string'],
+            'sex' => ['required', 'in:male,female'],
+            'date_of_birth' => ['nullable', 'date', 'date_format:Y-m-d'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', Password::default()],
+        ];
+    }
 
-  public function save(): bool
-  {
-      if ($this->user->is_first_time) {
-          $this->validate([
-              'password' => ['required', 'string', Password::default()],
-          ]);
-      }
+    public function messages(): array
+    {
+        return [
+            // Basic information
+            'first_name.required' => 'The first name field is required.',
+            'middle_name.string' => 'The middle name field must be a string.',
+            'last_name.required' => 'The last name field is required.',
+            'sex.required' => 'The gender field is required.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'The email field must be a valid email.',
+            'email.unique' => 'The email field has already been taken.',
+            'email.exists' => 'The email field is invalid.',
+            'password.required' => 'The password field is required.',
+        ];
+    }
 
-      $this->validate();
+    public function save(): bool
+    {
+        if ($this->user->is_first_time) {
+            $this->validate([
+                'password' => ['required', 'string', Password::default()],
+                'photo' => 'nullable|image|max:2048',
+            ]);
+        }
 
-      DB::beginTransaction();
+        $this->validate();
 
-      try {
-          if ($this->user->is_first_time) {
-              $this->user->update([
-                  'generated_password' => $this->password,
-                  'password' => bcrypt($this->password),
-              ]);
-          }
+        DB::beginTransaction();
 
-          $this->user->update([
-              'last_name' => $this->last_name,
-              'first_name' => $this->first_name,
-              'middle_name' => $this->middle_name,
-              'email' => $this->email,
-              'status' => $this->user_status,
-              'sex' => $this->sex,
-              'updated_at' => now(),
-          ]);
+        try {
+            if ($this->staff->user->is_first_time) {
+                $this->staff->user->update([
+                    'generated_password' => $this->password,
+                    'password' => bcrypt($this->password),
+                ]);
+            }
 
-          DB::commit();
+            $this->staff->user->update([
+                'last_name' => $this->last_name,
+                'first_name' => $this->first_name,
+                'middle_name' => $this->middle_name,
+                'email' => $this->email,
+                'status' => $this->user_status,
+                'sex' => $this->sex,
+                'date_of_birth' => $this->date_of_birth,
+                'updated_at' => now(),
+            ]);
 
-          return true;
-      } catch (Exception $e) {
-          DB::rollback();
+            if ($this->photo) {
+                $existingPhoto = UserProfilePhoto::where('user_id', $this->user->id)->first();
+                if ($existingPhoto) {
+                    Storage::delete($existingPhoto->photo_path);
+                    $existingPhoto->delete();
+                }
+        
+                $path = $this->photo->store('profile_photos');
+                UserProfilePhoto::create([
+                    'user_id' => $this->user->id,
+                    'photo_path' => $path,
+                ]);
+            }
 
-          return false;
-      }
+            DB::commit();
 
-  }
+            return true;
+        } catch (Exception $e) {
+            DB::rollback();
 
-//   protected function isRootComponent() {}
+            return false;
+        }
+    }
+
+    //   protected function isRootComponent() {}
 
     public function render()
     {
