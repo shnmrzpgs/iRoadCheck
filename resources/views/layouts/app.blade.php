44dab24166a4cfb0dc52e7a85add4c0977b1aa76
Marian Joy G. Corpuz <mjgcorpuz00362@usep.edu.ph>
2025-02-19 03:07:40 +0800

    perf: added changes in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,10 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ $title }}</title>
-        @laravelPWA
+{{--        @laravelPWA--}}
         <script src="{{ asset('resources/js/app.js') }}"></script>
         <script src="{{ asset('js/map.js') }}"></script>
+        <script src="{{ asset('geoJSON/tagumCityRoad.js') }}"></script>
 
 
         <!-- Favicons -->
