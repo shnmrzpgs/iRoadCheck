4b8b5fa1f66384a0255c81247d2d6101c94d1bf4
Marian Joy G. Corpuz <mjgcorpuz00362@usep.edu.ph>
2025-03-26 10:19:15 +0800

    refactor: change severity column to label

--- database/migrations/2024_11_12_163341_create_severities_table.php
+++ database/migrations/2024_11_12_163341_create_severities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('severities', function (Blueprint $table) {
             $table->id();
-            $table->string('severity');
+            $table->string('label');
         });
     }
 
