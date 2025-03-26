3f0467d0aa2aa62a8149e69e1c94bec4cb7e2293
Marian Joy G. Corpuz <mjgcorpuz00362@usep.edu.ph>
2025-03-26 10:09:25 +0800

    feat: search bar component

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,12 +1,20 @@
-<div class="flex w-80 items-center px-5">
-    <form class="relative flex flex-1 h-8 rounded-full border-[#F8F7F7]" action="{{ $action }}" method="{{ $method }}">
-        <label for="search-field" class="sr-only">Search</label>
-        <svg class="pointer-events-none absolute inset-y-0 left-1 h-full w-4 text-gray-400 ml-2 z-10" viewBox="0 0 20 20" fill="#6AA76F" aria-hidden="false">
-            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-        </svg>
-        <input id="search-field"
-               class="border border-gray-300 focus:outline-none focus:ring-[0.5px] focus:ring-[#6AA76F] focus:border-[#6AA76F] drop-shadow-md focus:bg-white bg-white rounded-full border-none block h-full w-full py-0 pl-8 text-gray-900 placeholder:text-gray-400"
-               placeholder="{{ $placeholder }}" type="search" name="search">
+{{--@if (!request()->routeIs('admin.profile-edit') && !request()->routeIs('admin.road-defect-reports') && !session('hideSearchBar'))--}}
+@if (!$hideSearchBar)
+    <form action="{{ $action }}" method="{{ $method }}" class="flex mt-2 lg:mt-3 w-48 lg:w-80 items-center px-0 lg:px-5">
+        <div class="relative flex flex-1 h-8 rounded-full">
+            <label for="search-field" class="sr-only">Search</label>
+            <svg class="pointer-events-none absolute inset-y-0 left-1 h-full w-4 text-gray-400 ml-2 z-0"
+                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#4AA76F">
+                <path d="M368 208A160 160 0 1 0 48 208a160 160 0 1 0 320 0zM337.1 371.1C301.7 399.2 256.8 416 208 416C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208c0 48.8-16.8 93.7-44.9 129.1l124 124 17 17L478.1 512l-17-17-124-124z"/>
+            </svg>
+            <input
+                id="search-field"
+                class="border border-gray-300 focus:outline-none focus:ring-[0.5px] focus:ring-[#4AA76F] focus:border-[#4AA76F] shadow-[0px_1px_5px_rgba(0,0,0,0.2)] focus:bg-white bg-white rounded-full block h-full w-full py-0 pl-8 text-gray-900 placeholder:text-gray-400 text-xs lg:text-[14px]"
+                wire:model.live="search"
+                placeholder="{{ $placeholder ?? 'Search...' }}"
+                type="search"
+            />
+        </div>
     </form>
-</div>
+@endif
 
