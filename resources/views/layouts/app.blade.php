c40480b355747e2d3669d3c993568eac88983d39
Marian Joy G. Corpuz <mjgcorpuz00362@usep.edu.ph>
2025-03-26 10:18:37 +0800

    refactor: comment .js script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,13 @@
 
         <title>{{ $title }}</title>
 {{--        @laravelPWA--}}
-        <script src="{{ asset('resources/js/app.js') }}"></script>
-        <script src="{{ asset('js/map.js') }}"></script>
-        <script src="{{ asset('geoJSON/tagumCityRoad.json') }}"></script>
-
+{{--        <script src="{{ asset('resources/js/app.js') }}"></script>--}}
+{{--        <script src="{{ asset('js/map.js') }}"></script>--}}
+{{--        <script src="{{ asset('geoJSON/tagumCityRoad.js') }}"></script>--}}
 
         <!-- Favicons -->
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/images/IRoadCheck_Logo.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('storage/images/IRoadCheck_Logo.png') }}">
-
 
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
