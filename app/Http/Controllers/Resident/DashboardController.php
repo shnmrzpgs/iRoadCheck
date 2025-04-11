25848f7e472b0107db32b4a1e0d2a4df524d14b5
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-12 05:56:59 +0800

    Testing to fix

--- app/Http/Controllers/Resident/DashboardController.php
+++ app/Http/Controllers/Resident/DashboardController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
 
-    public function __invoke()
+    public function index()
     {
         return view('resident.pages.dashboard');
     }
