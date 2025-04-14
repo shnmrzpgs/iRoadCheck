452809782de1237f4c750af499e80ea9bb4208b5
Marian Joy G. Corpuz <mjgcorpuz00362@usep.edu.ph>
2025-04-15 01:46:41 +0800

    feat: added must_change_password column in users table

--- database/migrations/2024_01_08_064821_create_users_table.php
+++ database/migrations/2024_01_08_064821_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->boolean('first_login')->default(true);
+            $table->boolean('must_change_password')->default(true);
             $table->string('username')->nullable()->unique();
             $table->string('first_name');
             $table->string('middle_name')->nullable();
