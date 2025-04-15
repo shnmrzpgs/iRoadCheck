0400fc133f9bc3ced6c989a2c081d6651d4ae0b7
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-16 02:49:44 +0800

    [VPS] Fixed update report

--- app/Livewire/ReviewUpdate.php
+++ app/Livewire/ReviewUpdate.php
@@ -49,7 +49,7 @@ use Livewire\Component;
     ) AS distance
 ")
                         ->setBindings([$lng, $lat])
-                        ->having("distance", "<=", 500) // in meters
+                        ->having("distance", "<=", 5) // in meters
                         ->where("status", "Unfixed")
                         ->orderBy("distance")
                         ->get();
