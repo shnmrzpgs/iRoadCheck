f076a7460b32f3b98bcfd5a6db0cb00bf8e2b8ca
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-16 02:24:09 +0800

    [VPS] Changed meter to 1

--- app/Livewire/ReviewUpdate.php
+++ app/Livewire/ReviewUpdate.php
@@ -49,7 +49,7 @@ use Livewire\Component;
     ) AS distance
 ")
                         ->setBindings([$lng, $lat])
-                        ->having("distance", "<=", 5) // in meters
+                        ->having("distance", "<=", 1) // in meters
                         ->where("status", "Unfixed")
                         ->orderBy("distance")
                         ->get();
