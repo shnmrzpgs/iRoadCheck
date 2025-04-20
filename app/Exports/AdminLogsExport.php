bef0a52a83fc2831c3afc6ec7a766250e57d48be
shnmrzpgs <145262229+shnmrzpgs@users.noreply.github.com>
2025-04-20 14:15:41 +0800

    feat: add encryption/decryption function on Exports

--- app/Exports/AdminLogsExport.php
+++ app/Exports/AdminLogsExport.php
@@ -9,6 +9,7 @@ use Maatwebsite\Excel\Concerns\WithDrawings;
 use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use Illuminate\Support\Facades\Crypt;
 
 class AdminLogsExport implements FromView, WithEvents, WithDrawings
 {
@@ -18,6 +19,7 @@ class AdminLogsExport implements FromView, WithEvents, WithDrawings
     public function __construct($filteredAdminLogs)
     {
         $this->filteredAdminLogs = $filteredAdminLogs;
+        
     }
 
 
