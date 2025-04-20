30cedd7fcc23e28ce31980475b7d20ea9755c6ca
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-20 16:08:45 +0800

    [VPS] Fix if error

--- resources/views/livewire/modals/admin/users-modal/edit-user-account-modal.blade.php
+++ resources/views/livewire/modals/admin/users-modal/edit-user-account-modal.blade.php
@@ -442,7 +442,7 @@
                                 </div>
                             </div>
                         </div>
-                        @endif
+
 
                         </div>
                     </div>
