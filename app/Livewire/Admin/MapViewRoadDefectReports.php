fcb3ed6b2a29935748df4d1aa71da6317b80f27a
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-22 05:13:25 +0800

    [VPS] Added redirection

--- app/Livewire/Admin/MapViewRoadDefectReports.php
+++ app/Livewire/Admin/MapViewRoadDefectReports.php
@@ -47,7 +47,7 @@ class MapViewRoadDefectReports extends Component
                     : null;
 
                 // Severity label
-                $report->severity_label = $report->severity->label ?? 'Unknown';
+                $report->severity_label = $report->severity ?? 'Unknown';
 
                 return $report;
             })
