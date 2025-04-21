c1971d6d102c2d96cba8969ec5f843ad9cbcf54e
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-22 06:43:32 +0800

    bugged

--- resources/views/livewire/admin/map-view-road-defect-reports.blade.php
+++ resources/views/livewire/admin/map-view-road-defect-reports.blade.php
@@ -1304,7 +1304,6 @@
                         report.location?.toLowerCase(),
                         report.status?.toLowerCase(),
                         report.date?.toLowerCase(),
-                        report.severity_label.toLowerCase(),
                     ].some(field => field?.includes(searchQuery));
 
                     const reportDateStr = report.date_reported || report.formatted_date || report.date;
