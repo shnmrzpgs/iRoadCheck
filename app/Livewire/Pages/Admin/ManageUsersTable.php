043f2577222cdf42e3748885ccd6520d7196c6df
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-04-21 09:23:37 +0800

    [VPS] Fixed profile

--- app/Livewire/Pages/Admin/ManageUsersTable.php
+++ app/Livewire/Pages/Admin/ManageUsersTable.php
@@ -207,7 +207,7 @@ public function exportStaffs()
     foreach ($staffs as $staff) {
         if ($staff->user) {
             $staff->user->first_name = Crypt::decryptString($staff->user->first_name);
-//            $staff->user->middle_name = Crypt::decryptString($staff->user->middle_name);
+            $staff->user->middle_name = Crypt::decryptString($staff->user->middle_name);
             $staff->user->last_name = Crypt::decryptString($staff->user->last_name);
             $staff->user->username = Crypt::decryptString($staff->user->username);
         }
