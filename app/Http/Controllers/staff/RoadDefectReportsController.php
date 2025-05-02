b1c353a175498a46107bf48939665827b9550ca2
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-05-02 10:49:02 +0800

    [VPS] Test roles

--- app/Http/Controllers/staff/RoadDefectReportsController.php
+++ app/Http/Controllers/staff/RoadDefectReportsController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers\staff;
 
 use App\Http\Controllers\Controller;
+use App\Models\Staff;
+use App\Models\StaffRolesPermissions;
 use Illuminate\Http\Request;
 
 class RoadDefectReportsController extends Controller
 {
     public function index()
     {
-        return view('staff.pages.road-defect-reports');
+        $user = \Auth::user();
+        $staff = Staff::where('user_id', $user->id)->first();
+        $roleid = $staff->staffRolesPermissions->staffRole->id;
+        $get = StaffRolesPermissions::where('staff_role_id', $roleid)->get();
+
+        foreach ($get as $item) {
+            if ($item->staff_permission_id == 3) {
+                return view('staff.pages.road-defect-reports');
+            }
+            else{
+                return route('staff.report-history');
+            }
+        }
     }
 
 }
